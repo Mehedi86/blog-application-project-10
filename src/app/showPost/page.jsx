76259a21dcde51f7b8a43ac97d294e0746be9fda
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-07-26 12:54:20 +0600

    added: get ready the post page

--- src/app/showPost/page.jsx
+++ src/app/showPost/page.jsx
@@ -2,39 +2,39 @@
 import React, { useEffect, useState } from "react";
 
 export default function Page() {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const res = await fetch("/api/showPost");
-        const data = await res.json();
-        setPosts(data);
-      } catch (err) {
-        console.error("Error fetching posts:", err);
-      }
-    }
-    fetchPosts();
-  }, []);
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const res = await fetch("/api/showPost");
+                const data = await res.json();
+                setPosts(data);
+            } catch (err) {
+                console.error("Error fetching posts:", err);
+            }
+        }
+        fetchPosts();
+    }, []);
 
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">📜 All Posts</h1>
-      {posts.length === 0 ? (
-        <p>No posts found</p>
-      ) : (
-        <div className="space-y-4">
-          {posts.map((post) => (
-            <div key={post._id} className="border p-4 rounded shadow">
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p className="text-gray-700">{post.content}</p>
-              <span className="text-sm text-gray-500">
-                {new Date(post.timestamp).toLocaleString()}
-              </span>
-            </div>
-          ))}
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">📜 All Posts</h1>
+            {posts.length === 0 ? (
+                <p>No posts found</p>
+            ) : (
+                <div className="space-y-4">
+                    {posts.map((post) => (
+                        <div key={post._id} className="border-2 border-neutral-200  p-4 rounded shadow space-y-4">
+                            <h2 className="text-xl font-semibold text-center bg-rose-100 rounded-lg p-4 w-1/3 mx-auto">{post.title}</h2>
+                            <p className="text-neutral-700 font-semibold text-center bg-teal-100 rounded-lg p-4 w-1/2 mx-auto">{post.content}</p>
+                            <span className="text-sm text-gray-500">
+                                <p><span className="font-bold text-black">Created At:</span> {new Date(post.timestamp).toLocaleString()}</p>
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
